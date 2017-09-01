df6f6317597afebc6d2026433c51a4bfb8641a72
Daniel Tapia <dtapian94@gmail.com>
2017-09-01 13:08:41 -0500

    Add files via upload

---
+++ Evaluacion 1/Scanner.cpp
@@ -0,0 +1,10 @@
+#include "Scanner.h"
+
+int main(int argc, char** argv){
+    Scanner s;
+    std::vector<int> cosa = s.getOpenPortList();
+
+    for(int i: cosa){
+        std::cout << "OPEN: " << i << std::endl;
+    }
+}
